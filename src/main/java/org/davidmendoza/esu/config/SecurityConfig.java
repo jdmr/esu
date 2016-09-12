efc9e6b40788cb9c2eac0ac06a7b5938a5b33688
J. David Mendoza <jdmendozar@gmail.com>
2016-09-12 09:10:04 -0500

    Working on login flow

--- src/main/java/org/davidmendoza/esu/config/SecurityConfig.java
+++ src/main/java/org/davidmendoza/esu/config/SecurityConfig.java
@@ -23,16 +23,32 @@
  */
 package org.davidmendoza.esu.config;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import javax.servlet.Filter;
+import org.davidmendoza.esu.dao.RolRepository;
+import org.davidmendoza.esu.dao.UsuarioRepository;
+import org.davidmendoza.esu.shared.Rol;
+import org.davidmendoza.esu.shared.Usuario;
+import org.davidmendoza.esu.utils.LoginHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
@@ -40,6 +56,7 @@ import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilt
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CompositeFilter;
 
 /**
  *
@@ -49,8 +66,17 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Autowired
-    OAuth2ClientContext oauth2ClientContext;
+    private OAuth2ClientContext oauth2ClientContext;
+
+    @Autowired
+    private LoginHandler loginHandler;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private RolRepository rolRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -68,11 +94,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     private Filter ssoFilter() {
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+
         OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
         OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
         facebookFilter.setRestTemplate(facebookTemplate);
         facebookFilter.setTokenServices(new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
-        return facebookFilter;
+        facebookFilter.setAuthenticationSuccessHandler(loginHandler);
+        filters.add(facebookFilter);
+
+        OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/google");
+        OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oauth2ClientContext);
+        googleFilter.setRestTemplate(googleTemplate);
+        UserInfoTokenServices uits = new UserInfoTokenServices(googleResource().getUserInfoUri(), google().getClientId());
+        uits.setPrincipalExtractor(googlePrincipalExtractor(usuarioRepository, rolRepository));
+        uits.setAuthoritiesExtractor(googleAuthoritiesExtractor(usuarioRepository));
+        googleFilter.setTokenServices(uits);
+        googleFilter.setAuthenticationSuccessHandler(loginHandler);
+        filters.add(googleFilter);
+
+        filter.setFilters(filters);
+        return filter;
     }
 
     @Bean
@@ -88,12 +131,100 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @ConfigurationProperties("google.client")
+    public AuthorizationCodeResourceDetails google() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.resource")
+    public ResourceServerProperties googleResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
     public FilterRegistrationBean oauth2ClientFilterRegistration(
             OAuth2ClientContextFilter filter) {
         FilterRegistrationBean registration = new FilterRegistrationBean();
         registration.setFilter(filter);
         registration.setOrder(-100);
         return registration;
+    }
+
+    public PrincipalExtractor googlePrincipalExtractor(UsuarioRepository usuarioRepository, RolRepository rolRepository) {
+        return map -> {
+            log.debug("Google extractor");
+            for (String key : map.keySet()) {
+                log.debug("{} : {}", key, map.get(key));
+            }
+
+            List<Map<String, String>> emails = (List<Map<String, String>>) map.get("emails");
+            Map<String, String> email = emails.get(0);
+            String username = email.get("value");
+            Usuario usuario = usuarioRepository.findByUsernameIgnoreCase(username);
+            if (usuario == null) {
+                log.info("Deberia crear usuario {}", username);
+                Date date = new Date();
+                Map<String, String> name = (Map<String, String>) map.get("name");
+                usuario = new Usuario();
+                usuario.setUsername(username);
+                usuario.setNombre(name.get("givenName"));
+                usuario.setApellido(name.get("familyName"));
+                Rol rol = rolRepository.findByAuthority("ROLE_USER");
+                usuario.getRoles().add(rol);
+                usuario.setDateCreated(date);
+                usuario.setLastUpdated(date);
+                usuario.setPassword(UUID.randomUUID().toString());
+                usuario = usuarioRepository.save(usuario);
+            }
+            return usuario;
+        };
+    }
+
+    public AuthoritiesExtractor googleAuthoritiesExtractor(UsuarioRepository usuarioRepository) {
+        return map -> {
+            log.debug("Google Authorities Extractor");
+            List<Map<String, String>> emails = (List<Map<String, String>>) map.get("emails");
+            Map<String, String> email = emails.get(0);
+            String username = email.get("value");
+            log.debug("Getting {} authorities", username);
+            Usuario usuario = usuarioRepository.findByUsernameIgnoreCase(username);
+            if (usuario != null) {
+                String roles = "";
+                for (Rol rol : usuario.getRoles()) {
+                    if (roles.length() > 0) {
+                        roles += ",";
+                    }
+                    roles += rol.getAuthority();
+                }
+                switch (roles) {
+                    case "ROLE_ADMIN":
+                        roles += ",ROLES_EDITOR";
+                    case "ROLE_EDITOR":
+                        roles += ",ROL_AUTOR";
+                    case "ROLE_AUTOR":
+                        roles += ",ROLE_USER";
+                }
+                return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+            }
+            throw new BadCredentialsException("El usuario no existe");
+        };
+    }
+
+    public PrincipalExtractor facebookPrincipalExtractor(UsuarioRepository usuarioRepository) {
+        return map -> {
+            log.debug("Facebook extractor");
+            for (String key : map.keySet()) {
+                log.debug("{} : {}", key, map.get(key));
+            }
+
+            String username = (String) map.get("login");
+            Usuario speaker = usuarioRepository.findByUsernameIgnoreCase(username);
+            if (speaker == null) {
+                log.info("Deberia crear usuario {}", username);
+            }
+            return speaker;
+        };
     }
 
 }
--- src/main/java/org/davidmendoza/esu/dao/RolRepository.java
+++ src/main/java/org/davidmendoza/esu/dao/RolRepository.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2015 J. David Mendoza.
+ * Copyright 2016 Universidad de Montemorelos.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,28 +23,14 @@
  */
 package org.davidmendoza.esu.dao;
 
-import java.util.List;
-import org.davidmendoza.esu.shared.Usuario;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.davidmendoza.esu.shared.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author J. David Mendoza <jdmendoza@swau.edu>
  */
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    public Usuario findByUsernameIgnoreCase(String username);
+public interface RolRepository extends JpaRepository<Rol, Long> {
     
-    public Page<Usuario> findByUsernameLikeOrNombreLikeOrApellidoLikeAllIgnoreCase(String username, String nombre, String apellido, Pageable pageable);
-    
-    @Query("select u from Usuario u where upper(u.nombre) like :filtro or upper(u.apellido) like :filtro order by u.nombre, u.apellido")
-    public List<Usuario> busca(@Param("filtro")String filtro);
-    
-    @Query("select new Usuario( u.password, u.dateCreated ) from Usuario u where u.id = :usuarioId")
-    public Usuario dateCreated(@Param("usuarioId") Long usuarioId);
-    
+    public Rol findByAuthority(String authority);
 }
